ef0fd022fd38a170dcc0ae95bc657af8c3ea5e1b
eduardo <eduardolazaroso@hotmail.com>
2023-01-17 08:00:13 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        HeroDetailComponent,
+        DashboardComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        CoreModule,
+    ]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/core/services/moc-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "../models/hero.model";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolverine' },
+  { id: 2, name: 'Mulher Maravilha' },
+  { id: 3, name: 'Superman' },
+  { id: 4, name: 'Homem Aranha' },
+  { id: 5, name: 'Aquamen' },
+  { id: 6, name: 'Pantera Negra' },
+];
